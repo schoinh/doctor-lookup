c21314d37cbe8b7082c049e65fb09de873d6c7bc
Na Hyung Choi <schoinh@gmail.com>
2019-07-05 15:43:10 -0700

    start making room for search by name

--- src/api-calls.js
+++ src/api-calls.js
@@ -1,8 +1,24 @@
 export class DoctorSearch {
-  getDoctors(keyword) {
+  getDoctorsByKeyword(keyword) {
     return new Promise(function(resolve, reject) {
       const request = new XMLHttpRequest();
       const url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${keyword}&location=47.609207,-122.336677,5&user_location=47.609207,-122.336677&sort=best-match-desc&limit=30&user_key=${process.env.apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+
+  getDoctorsByName(name) {
+    return new Promise(function(resolve, reject) {
+      const request = new XMLHttpRequest();
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=47.609207,-122.336677,5&user_location=47.609207,-122.336677&sort=best-match-desc&limit=30&user_key=${process.env.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
