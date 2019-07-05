1b57248000856e1627c002dba86ed025e5ce77db
Na Hyung Choi <schoinh@gmail.com>
2019-07-05 13:49:08 -0700

    list address of doctor in results

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import { DoctorSearch } from './api-calls';
 $(function() {
   $('form').submit(function(event) {
     event.preventDefault();
-    $('.errors, .output').empty();
+    $('.errors, ol').empty();
 
     const inputKeyword = $('#keyword').val();
     
@@ -19,8 +19,22 @@ $(function() {
         const output = JSON.parse(result);
         const doctorList = output.data;
         for (var i = 0; i < doctorList.length; i++) {
-          const nameFormatted = `${doctorList[i].profile.first_name} ${doctorList[i].profile.last_name}, ${doctorList[i].profile.title}`
-          $('ol').append(`<li>${nameFormatted}</li>`);
+          const fullName = `${doctorList[i].profile.first_name} ${doctorList[i].profile.last_name}, ${doctorList[i].profile.title}`;
+          let addressInfo;
+          for (var j = 0; j < (doctorList[i].practices).length; j++) {
+            if (doctorList[i].practices[j].within_search_area === true && doctorList[i].practices[j].visit_address) {
+            addressInfo = doctorList[i].practices[j].visit_address;
+            break;
+            } else {
+              addressInfo = false;
+            }
+          }
+          if (addressInfo) {
+            const addressOutput = `${addressInfo.street}<br>${addressInfo.city}, ${addressInfo.state} ${addressInfo.zip}`
+            $('ol').append(`<li>${fullName}<br>${addressOutput}</li>`);
+          } else {
+            $('ol').append(`<li>${fullName}<br>No address on file</li>`);
+          }
         }
       })
       .catch(function(error) {
