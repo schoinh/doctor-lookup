0cafae38ce431052f5b6350a2d2ddae5dfa36886
Na Hyung Choi <schoinh@gmail.com>
2019-07-05 09:26:15 -0700

    update project name in webpack files

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'Project Name',      // CHANGE THIS
+      title: 'Doctor Lookup',      
       template: './src/index.html',
       inject: 'body'
     }),
