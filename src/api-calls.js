12451545ee75dc54b75359ffe92d8db07091eabf
Na Hyung Choi <schoinh@gmail.com>
2019-07-05 12:06:28 -0700

    add getDoctors API call

---
+++ src/api-calls.js
@@ -0,0 +1,17 @@
+export class DoctorSearch {
+  getDoctors(keyword) {
+    return new Promise(function(resolve, reject) {
+      const request = new XMLHttpRequest();
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${keyword}&location=47.609207,-122.336677,5&user_location=47.609207,-122.336677&sort=best-match-desc&limit=30&user_key=${process.env.apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
